83179c900e421445c813336edd5cf02562ccdca9
Giang-Nguyen-2005 <nguyengiang2005a@gmail.com>
2025-07-06 18:09:54 +0700

    final update nếu không lỗi :D

--- templates/chi_tiet_bai_viet.php
+++ templates/chi_tiet_bai_viet.php
@@ -3,7 +3,9 @@ include '../includes/header.php';
 
 $link = mysqli_connect("localhost", "root", "", "travel_binh_dinh", 3307);
 $id = isset($_GET['id']) ? intval($_GET['id']) : 0;
-$sql = "SELECT * FROM bai_viet WHERE id = $id LIMIT 1";
+$sql = "SELECT bv.*, u.ho_ten FROM bai_viet bv 
+        LEFT JOIN user u ON bv.id_user = u.id 
+        WHERE bv.id = $id LIMIT 1";
 $result = mysqli_query($link, $sql);
 
 if ($row = mysqli_fetch_array($result)) {
@@ -16,6 +18,11 @@ if ($row = mysqli_fetch_array($result)) {
     <section class="content-wrapper">
         <div class="text-left-content">
             <h1><?php echo $row['tieu_de']; ?></h1>
+            <div style="font-size: 13px; color:rgb(136, 127, 127);text-align: right;">
+                <p>Người đăng: <span style="font-weight:bold;color:rgb(101, 0, 0)"><?php echo $row['ho_ten']; ?></span></p>
+                <p>Ngày đăng: <?php echo date("d/m/Y", strtotime($row['ngay_tao'])); ?></p>
+
+            </div>
             <p><?php echo $row['noi_dung']; ?></p>
         </div>
 
@@ -30,7 +37,7 @@ if ($row = mysqli_fetch_array($result)) {
                 <div class="mini-card">
                     <img src="../assets/img/<?php echo $khac['hinh_anh']; ?>" alt="<?php echo $khac['tieu_de']; ?>">
                     <div class="mini-card-content">
-                        <h4><?php echo htmlspecialchars($khac['tieu_de']); ?></h4>
+                        <h4><?php echo ($khac['tieu_de']); ?></h4>
                         <a href="chi_tiet_bai_viet.php?id=<?php echo $khac['id']; ?>">Xem chi tiết →</a>
                     </div>
                 </div>
